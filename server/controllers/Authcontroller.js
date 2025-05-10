082385743a5b85393570896fe4c9d1315f7723a1
Pankajnegi06 <pankajsnegi06@gmail.com>
2025-05-10 11:17:21 +0530

    Bcrypt

--- server/controllers/Authcontroller.js
+++ server/controllers/Authcontroller.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { fileUpload } from "../config/cloudinary.js";
 import { User } from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 import { Message } from "../models/message.model.js";
 
