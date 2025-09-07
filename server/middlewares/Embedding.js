fa4ceca71a5d38b81533b209178061af95add8ca
Pankajnegi06 <pankajsnegi06@gmail.com>
2025-09-07 11:37:35 +0530

    new changes

--- server/middlewares/Embedding.js
+++ server/middlewares/Embedding.js
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 import { Message } from "../models/message.model.js";
+import dotenv from 'dotenv';
+dotenv.config();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 
 async function generateEmbeddingForMessage(message) {
